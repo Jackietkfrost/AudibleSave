986a3bd5f0e21b7cb827398da9a5bff32ed49130
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2024-07-23 00:44:26 -0400

    Update for custom audio
    - Added option for custom audio to play.
    - Added second custom sound
    - Added beginning of audio volume management. (Is that even possible?)

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import player from 'node-wav-player';
+const player = require('node-wav-player');
 import * as fs from 'fs';
 import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-	let selectedAudioFile: string = 'pop.wav';
-	let showNotifs = true;
+	let selectedAudioFile: string = vscode.workspace.getConfiguration("audible-save").get("audioFiles") as string;
+	let showNotifs = vscode.workspace.getConfiguration("audible-save").get("showNotifications") as boolean;
+	const audioVolume = vscode.workspace.getConfiguration("audible-save").get("audioVolume") as number;
 
+	if (selectedAudioFile === "custom") {
+		selectedAudioFile = vscode.workspace.getConfiguration("audible-save").get("customAudio") as string;
+	}
+
+	function addAudio() {
+		const audioDirPath = path.join(context.extensionPath, 'audio');
+
+		vscode.window.showOpenDialog({
+			canSelectFiles: true,
+			canSelectFolders: false,
+			canSelectMany: false,
+			openLabel: 'Select Audio File',
+			defaultUri: vscode.Uri.file(audioDirPath)
+		}).then((fileUri) => {
+			if (fileUri && fileUri[0]) {
+				const selectedAudioFile = fileUri[0].fsPath;
+				const fileName = path.basename(selectedAudioFile);
+				const destinationPath = path.join(audioDirPath, fileName);
+
+				fs.copyFile(selectedAudioFile, destinationPath, (err) => {
+					if (err) {
+						console.error('Error copying file:', err);
+					} else {
+						vscode.workspace.getConfiguration("audible-save").update("audioFiles", selectedAudioFile, vscode.ConfigurationTarget.Global);
+					}
+				});
+			}
+		});
+	}
 
 	function getAudioFiles(): Promise<string[]> {
 		const audioDirPath = path.join(context.extensionPath, 'audio');
@@ -31,17 +59,25 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 
 	getAudioFiles().then((audioFiles) => {
-		const enumValues = audioFiles.map((audioFile) => `"${audioFile}"`);
-		const enumItemLabels = audioFiles.map((audioFile) => audioFile.replace('.wav', ''));
+		// VS Code doesn't support dynamically loaded enums, so we can't do it this way.
+		// ref: https://github.com/microsoft/vscode/issues/187141
+		// const enumValues = audioFiles.map((audioFile) => `"${audioFile}"`);
+		// const enumItemLabels = audioFiles.map((audioFile) => audioFile.replace('.wav', ''));
+		// vscode.workspace.getConfiguration().update('audible-save.audioFiles', enumValues.join(','), vscode.ConfigurationTarget.Workspace);
+		// vscode.workspace.getConfiguration().update('audible-save.audioFiles.enumItemLabels', enumItemLabels, vscode.ConfigurationTarget.Global);
 	});
 
 
 	vscode.workspace.onDidChangeConfiguration((event) => {
 		if (event.affectsConfiguration('audible-save.audioFiles')) {
 			selectedAudioFile = vscode.workspace.getConfiguration("audible-save").get('audioFiles') as string;
-		}
-		if (event.affectsConfiguration('audible-save.notification')) {
-			showNotifs = vscode.workspace.getConfiguration("audible-save").get('notification') as boolean;
+			if (selectedAudioFile === 'custom') {
+				const customAudioFile = vscode.workspace.getConfiguration("audible-save").get('customAudio') as string;
+				selectedAudioFile = customAudioFile;
+			}
+			if (event.affectsConfiguration('audible-save.notification')) {
+				showNotifs = vscode.workspace.getConfiguration("audible-save").get('notification') as boolean;
+			}
 		}
 	});
 
@@ -50,6 +86,7 @@ export function activate(context: vscode.ExtensionContext) {
 		const audioPath = path.join(context.extensionPath, 'audio', `${selectedAudioFile}`);
 		player.play({ path: audioPath }).catch((error) => {
 			console.error(`The file ${audioPath} was not found. Error:${error}`);
+			vscode.window.showErrorMessage(`The file ${audioPath} was not found. Error:${error}`);
 		});
 	}
 
@@ -68,8 +105,11 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		playAudio();
 	});
+	const addAudioFile = vscode.commands.registerCommand('audible-save.addAudio', () => {
+		addAudio();
+	});
 
-	context.subscriptions.push(manualPlay);
+	context.subscriptions.push(manualPlay, addAudioFile);
 }
 
 export function deactivate() { }
